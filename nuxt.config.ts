da8b960c7a16e51686cf9b8b2660da510ee6c446
matthew.n.adcock <matthew.n.adcock@gmail.com>
2024-05-28 15:29:52 +0000

    Update site config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/seo", "@nuxt/image", "nuxt-icons"],
   css: ["~/assets/styles/global.css"],
+  site: {
+    url: 'https://matt-is.gay',
+    name: 'Matt\'s Links',
+    description: 'Welcome to my corner of the internet - this is the portal to all things me :)',
+    defaultLocale: 'en'
+  },
+  seo: {
+    redirectToCanonicalSiteUrl: true
+  },
+  
 })
